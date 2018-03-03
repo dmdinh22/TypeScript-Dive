d3b9bd9a30fa3688e597dde6f3406e4c50eac7a7
David Dinh <dmdinh22@gmail.com>
2018-03-02 23:35:17 -0800

    completed JS section

--- JSisTS.ts
+++ JSisTS.ts
@@ -122,4 +122,74 @@ server.on(function handler(req, res) {
         // the `res` has been closed over and is available
         res.send(data);
     })
-});
+    });
+
+// NUMBER
+
+// Decimal
+console.log(.1 + .2); // 0.30000000000000004
+
+// Integer
+console.log({max: Number.MAX_SAFE_INTEGER, min: Number.MIN_SAFE_INTEGER});
+  // {max: 9007199254740991, min: -9007199254740991}
+
+// Safe ints
+console.log(Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2); // true!
+console.log(Number.MIN_SAFE_INTEGER - 1 === Number.MIN_SAFE_INTEGER - 2); // true!
+
+console.log(Number.MAX_SAFE_INTEGER);      // 9007199254740991
+console.log(Number.MAX_SAFE_INTEGER + 1);  // 9007199254740992 - Correct
+console.log(Number.MAX_SAFE_INTEGER + 2);  // 9007199254740992 - Rounded!
+console.log(Number.MAX_SAFE_INTEGER + 3);  // 9007199254740994 - Rounded - correct by luck
+console.log(Number.MAX_SAFE_INTEGER + 4);  // 9007199254740996 - Rounded!
+
+// check safety with ES6 Number.isSafeInteger
+// Safe value
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER)); // true
+
+// Unsafe value
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1)); // false
+
+// Because it might have been rounded to it due to overflow
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 10)); // false
+
+import { Big } from 'big.js';
+
+export const foo = new Big('111.11111111111111111111');
+export const bar = foo.plus(new Big('0.00000000000000000001'));
+
+// To get a number:
+const x: number = Number(bar.toString()); // Looses the precision
+
+// NaN
+console.log(Math.sqrt(-1)); //NaN
+
+// Don't do this
+console.log(NaN === NaN); // false!!
+
+// Do this
+console.log(Number.isNaN(NaN)); // true
+
+// Infinity
+console.log(Number.MAX_VALUE);  // 1.7976931348623157e+308
+console.log(-Number.MAX_VALUE); // -1.7976931348623157e+308
+
+console.log(Number.MAX_VALUE + 1 == Number.MAX_VALUE);   // true!
+console.log(-Number.MAX_VALUE - 1 == -Number.MAX_VALUE); // true!
+
+console.log(Number.MAX_VALUE + 10**1000);  // Infinity
+console.log(-Number.MAX_VALUE - 10**1000); // -Infinity
+
+console.log( 1 / 0); // Infinity
+console.log(-1 / 0); // -Infinity
+
+console.log(Number.POSITIVE_INFINITY === Infinity);  // true
+console.log(Number.NEGATIVE_INFINITY === -Infinity); // true
+
+console.log( Infinity >  1); // true
+console.log(-Infinity < -1); // true
+
+// Infinitesimal
+console.log(Number.MIN_VALUE);  // 5e-324
+
+console.log(Number.MIN_VALUE / 10);  // 0
